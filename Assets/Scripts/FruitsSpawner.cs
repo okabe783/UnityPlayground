ae3f13a46f49005777d942a2ee1add7b3c23272f
DAIBUTU\okayu <okayuya1113@gmail.com>
2026-02-04 23:23:52 +0900

    [add] 次のフルーツの画像を表示されるようにした

--- Assets/Scripts/FruitsSpawner.cs
+++ Assets/Scripts/FruitsSpawner.cs
@@ -1,9 +1,20 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FruitsSpawner : MonoBehaviour
 {
     public GameObject[] FruitsPrefabs;
     public Transform SpawnPoint;
+
+    public Image NextImage;
+    private int _currentIndex;
+    private int _nextIndex;
+    
+    private void Start()
+    {
+        _currentIndex = Random.Range(0, FruitsPrefabs.Length);
+        _nextIndex = Random.Range(0, FruitsPrefabs.Length);
+    }
     
     private void Update()
     {
@@ -20,7 +31,18 @@ public class FruitsSpawner : MonoBehaviour
 
     private void Spawn()
     {
-       Instantiate(FruitsPrefabs[Random.Range(0, 3)], SpawnPoint.position, Quaternion.identity);
+       Instantiate(FruitsPrefabs[_currentIndex], SpawnPoint.position, Quaternion.identity);
        GameManager.Instance.StartFruitFall();
+
+       _currentIndex = _nextIndex;
+       _nextIndex = Random.Range(0, FruitsPrefabs.Length);
+
+       UpdateNextUI();
+    }
+    
+    private void UpdateNextUI()
+    {
+        Fruits fruit = FruitsPrefabs[_nextIndex].GetComponent<Fruits>();
+        NextImage.sprite = fruit.GetComponent<SpriteRenderer>().sprite;
     }
 }
